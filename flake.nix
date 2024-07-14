3025d3431878a528c2873a9e18cfc32bef7d7b21
Pete Naveen Pearson <drone4four@gmail.com>
2024-07-14 07:10:18 +0000

    Upgrade from v23.11 to v24.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
